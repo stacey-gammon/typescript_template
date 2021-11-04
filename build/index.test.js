325b7a26e8bb5551bd4dce861491331401fb176b
Stacey Gammon <gammon@elastic.co>
2021-11-04 14:46:56 -0400

    remove node modules and build folder

--- build/index.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var _1 = require(".");
-it('test sayHello', function () {
-    expect((0, _1.sayHello)('World')).toBe('Hello World!');
-});
-//# sourceMappingURL=index.test.js.map
